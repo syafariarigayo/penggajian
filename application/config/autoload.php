b64ed96c345f9a5b5da9554b791ad626b84e5feb
syafariarigayo <syafari.ari@gmial.com>
2025-11-21 00:27:59 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('ModelPenggajian');
+$autoload['model'] = array('ModelPenggajian', 'ModelYayasan');
