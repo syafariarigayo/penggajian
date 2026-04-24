82fb5b4e663575ee20db49cde50ba4c4c29dab49
syafariarigayo <syafari.ari@gmial.com>
2026-04-24 18:59:48 +0700

    Update footer.php

--- application/views/template_admin/footer.php
+++ application/views/template_admin/footer.php
@@ -2,7 +2,6 @@
       <footer class="sticky-footer">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Syafari | Penggajian 2025</span>
           </div>
         </div>
       </footer>
