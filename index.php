ed112530772c829a4edc32c1347395410721bc17
syafariarigayo <syafari.ari@gmial.com>
2026-04-25 08:10:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ error_reporting(E_ALL);
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
