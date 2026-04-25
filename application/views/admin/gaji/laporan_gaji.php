f0d32fa3261d45e3fd684a5de2a8edc0d84fb602
syafariarigayo <syafari.ari@gmial.com>
2026-04-25 08:47:37 +0700

    Update laporan_gaji.php

--- application/views/admin/gaji/laporan_gaji.php
+++ application/views/admin/gaji/laporan_gaji.php
@@ -1,47 +1,50 @@
-<!-- Begin Page Content -->
-<div class="container-fluid">
-	<div class="card mx-auto" style="width: 35%">
-		<div class="card-header bg-primary text-white text-center">
-			FIlter Laporan Gaji Pegawai
-		</div>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-		<form method="POST" action="<?php echo base_url('admin/laporan_gaji/cetak_laporan_gaji')?>">
-		<div class="card-body">
-			<div class="form-group row">
-				<label for="inputPassword" class="col-sm-3 col-form-label">Bulan</label>
-				<div class="col-sm-9">
-					<select class="form-control" name="bulan">
-					    <option value=""> Pilih Bulan </option>
-					    <option value="01">Januari</option>
-					    <option value="02">Februari</option>
-					    <option value="03">Maret</option>
-					    <option value="04">April</option>
-					    <option value="05">Mei</option>
-					    <option value="06">Juni</option>
-					    <option value="07">Juli</option>
-					    <option value="08">Agustus</option>
-					    <option value="09">September</option>
-					    <option value="10">Oktober</option>
-					    <option value="11">November</option>
-					    <option value="12">Desember</option>
-				    </select>
-				</div>
-			</div>
+class Laporan_Gaji extends MY_Controller {
 
-			<div class="form-group row">
-				<label for="inputPassword" class="col-sm-3 col-form-label">Tahun</label>
-				<div class="col-sm-9">
-					<select class="form-control" name="tahun">
-					    <option value=""> Pilih Tahun </option>
-					    <?php $tahun = date('Y');
-					    for($i=2020;$i<$tahun+5;$i++) { ?>
-					    <option value="<?php echo $i ?>"><?php echo $i ?></option>
-					<?php }?>
-					</select>
-				</div>
-			</div>
-			<button style="width: 100%" type="submit" class="btn btn-primary"><i class="fas fa-print"></i>Cetak Laporan Gaji</button>
-	</div>
-	</form>
-</div>
-<!-- /.container-fluid -->
+    public function __construct(){
+        parent::__construct();
+        $this->cek_admin();
+    }
+
+    public function index()
+    {
+        $data['title'] = "Laporan Gaji Pegawai";
+        $this->load->view('template_admin/header', $data);
+        $this->load->view('template_admin/sidebar');
+        $this->load->view('admin/gaji/laporan_gaji');
+        $this->load->view('template_admin/footer');
+    }
+
+    public function cetak_laporan_gaji()
+    {
+        $data['title'] = "Cetak Laporan Gaji Pegawai";
+        $my_unit       = $this->get_my_unit();
+
+        if(isset($_GET['bulan']) && $_GET['bulan'] != '' && isset($_GET['tahun']) && $_GET['tahun'] != '') {
+            $bulan = $_GET['bulan'];
+            $tahun = $_GET['tahun'];
+        } else {
+            $bulan = date('m');
+            $tahun = date('Y');
+        }
+        $bulantahun  = $bulan . $tahun;
+        $unit_filter = $my_unit ? "AND dp.unit = '$my_unit'" : "";
+
+        $data['potongan']   = $this->ModelPenggajian->get_data('potongan_gaji')->result();
+        $data['cetak_gaji'] = $this->db->query(
+            "SELECT dp.nik, dp.nama_pegawai, dp.jenis_kelamin,
+                    dj.nama_jabatan, dj.gaji_pokok, dj.tj_transport, dj.uang_makan,
+                    dk.alpha
+             FROM data_pegawai dp
+             INNER JOIN data_kehadiran dk ON dk.nik = dp.nik
+             INNER JOIN data_jabatan dj ON dj.nama_jabatan = dp.jabatan
+             WHERE dk.bulan = '$bulantahun' $unit_filter
+             ORDER BY dp.nama_pegawai ASC"
+        )->result();
+
+        $this->load->view('template_admin/header', $data);
+        $this->load->view('admin/gaji/cetak_gaji', $data);
+    }
+}
