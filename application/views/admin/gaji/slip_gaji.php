9806097104bef403b414a883dbce6e00e3da7a71
syafariarigayo <syafari.ari@gmial.com>
2026-04-25 08:47:35 +0700

    Update slip_gaji.php

--- application/views/admin/gaji/slip_gaji.php
+++ application/views/admin/gaji/slip_gaji.php
@@ -1,60 +1,50 @@
-<!-- Begin Page Content -->
-<div class="container-fluid">
-	<div class="card mx-auto" style="width: 35%">
-		<div class="card-header bg-primary text-white text-center">
-			FIlter Slip Gaji Pegawai
-		</div>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-		<form method="POST" action="<?php echo base_url('admin/slip_gaji/cetak_slip_gaji')?>">
-		<div class="card-body">
-			<div class="form-group row">
-				<label for="inputPassword" class="col-sm-3 col-form-label">Bulan</label>
-				<div class="col-sm-9">
-					<select class="form-control" name="bulan">
-					    <option value=""> Pilih Bulan </option>
-					    <option value="01">Januari</option>
-					    <option value="02">Februari</option>
-					    <option value="03">Maret</option>
-					    <option value="04">April</option>
-					    <option value="05">Mei</option>
-					    <option value="06">Juni</option>
-					    <option value="07">Juli</option>
-					    <option value="08">Agustus</option>
-					    <option value="09">September</option>
-					    <option value="10">Oktober</option>
-					    <option value="11">November</option>
-					    <option value="12">Desember</option>
-				    </select>
-				</div>
-			</div>
+class Slip_Gaji extends MY_Controller {
 
-			<div class="form-group row">
-				<label for="inputPassword" class="col-sm-3 col-form-label">Tahun</label>
-				<div class="col-sm-9">
-					<select class="form-control" name="tahun">
-					    <option value=""> Pilih Tahun </option>
-					    <?php $tahun = date('Y');
-					    for($i=2020;$i<$tahun+5;$i++) { ?>
-					    <option value="<?php echo $i ?>"><?php echo $i ?></option>
-					<?php }?>
-					</select>
-				</div>
-			</div>
+    public function __construct(){
+        parent::__construct();
+        $this->cek_admin();
+    }
 
-			<div class="form-group row">
-				<label for="inputPassword" class="col-sm-3 col-form-label">Nama Pegawai</label>
-				<div class="col-sm-9">
-					<select class="form-control" name="nama_pegawai">
-					    <option value=""> Pilih Pegawai </option>
-					    <?php foreach($pegawai as $p) : ?>
-					    	<option value="<?php echo $p->nama_pegawai ?>"><?php echo $p->nama_pegawai ?></option>
-					    <?php endforeach; ?>
-					</select>
-				</div>
-			</div>
+    public function index()
+    {
+        $data['title'] = "Slip Gaji Pegawai";
+        $my_unit       = $this->get_my_unit();
 
-			<button style="width: 100%" type="submit" class="btn btn-primary"><i class="fas fa-print"></i>Cetak Slip Gaji</button>
-	</div>
-	</form>
-</div>
-<!-- /.container-fluid -->
+        if($my_unit) {
+            $data['pegawai'] = $this->db->where('unit', $my_unit)->get('data_pegawai')->result();
+        } else {
+            $data['pegawai'] = $this->ModelPenggajian->get_data('data_pegawai')->result();
+        }
+
+        $this->load->view('template_admin/header', $data);
+        $this->load->view('template_admin/sidebar');
+        $this->load->view('admin/gaji/slip_gaji', $data);
+        $this->load->view('template_admin/footer');
+    }
+
+    public function cetak_slip_gaji()
+    {
+        $data['title']    = "Cetak Slip Gaji Pegawai";
+        $data['potongan'] = $this->ModelPenggajian->get_data('potongan_gaji')->result();
+        $nama             = $this->input->post('nama_pegawai');
+        $bulan            = $this->input->post('bulan');
+        $tahun            = $this->input->post('tahun');
+        $bulantahun       = $bulan . $tahun;
+
+        $data['print_slip'] = $this->db->query(
+            "SELECT dp.nik, dp.nama_pegawai, dj.nama_jabatan,
+                    dj.gaji_pokok, dj.tj_transport, dj.uang_makan,
+                    dk.alpha, dk.bulan
+             FROM data_pegawai dp
+             INNER JOIN data_kehadiran dk ON dk.nik = dp.nik
+             INNER JOIN data_jabatan dj ON dj.nama_jabatan = dp.jabatan
+             WHERE dk.bulan = '$bulantahun' AND dk.nama_pegawai = '$nama'"
+        )->result();
+
+        $this->load->view('template_admin/header', $data);
+        $this->load->view('admin/gaji/cetak_slip_gaji', $data);
+    }
+}
